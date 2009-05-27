2a82e81d6d4cc9d2b02f4323148db22b59e4ce80
Nigel Soon <ns@openradical.com>
2009-05-27 12:54:55 +0930

    Remove absolute path

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 // symfony directories
-$sf_symfony_lib_dir  = '/Users/soon0009/Sites/sfprojects/ems/lib/vendor/symfony/lib';
-$sf_symfony_data_dir = '/Users/soon0009/Sites/sfprojects/ems/lib/vendor/symfony/data';
+$sf_symfony_lib_dir  = dirname(__FILE__).'/../lib/vendor/symfony/lib';
+$sf_symfony_data_dir = dirname(__FILE__).'/../lib/vendor/symfony/data';
