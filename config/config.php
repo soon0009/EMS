9650d948df66b7b2bd6d77be84f376c0ce4d651e
Nigel Soon <ns@openradical.com>
2009-05-27 12:39:56 +0930

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+// symfony directories
+$sf_symfony_lib_dir  = '/Users/soon0009/Sites/sfprojects/ems/lib/vendor/symfony/lib';
+$sf_symfony_data_dir = '/Users/soon0009/Sites/sfprojects/ems/lib/vendor/symfony/data';
