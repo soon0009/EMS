8c66de9f3e36fec2a4d16fa3f048cfeb1e300e8d
Nigel Soon <ns@openradical.com>
2009-05-29 16:45:38 +0930

    update functional test. Check guard is working against other links

--- test/functional/frontend/eventActionsTest.php
+++ test/functional/frontend/eventActionsTest.php
@@ -5,7 +5,6 @@ include(dirname(__FILE__).'/../../bootstrap/functional.php');
 // create a new test browser
 $browser = new sfTestBrowser();
 $browser->initialize();
-
 $browser->
   get('/')->
   isStatusCode(401)->
@@ -17,5 +16,22 @@ $browser->
   followRedirect()->
   isStatusCode(200)->
   isRequestParameter('action', 'list')->
-  checkResponseElement('body', '/Contemplating/')
+  checkResponseElement('body', '/Contemplating/')->
+  click('logout')->
+  isRequestParameter('module', 'sfGuardAuth')->
+  isRequestParameter('action', 'signout')->
+  isRedirected()->
+  followRedirect()->
+  isStatusCode(401)->
+  get('/event/trip-to-the-moon')->
+  isStatusCode(401)->
+  isRequestParameter('module', 'event')->
+  isRequestParameter('action', 'show')->
+  checkResponseElement('body', '/FAN:/')->
+  click('sign in', array('username'=>'soon0009', 'password'=>'*th3C0n*'))->
+  isRedirected()->
+  followRedirect()->
+  isStatusCode(200)->
+  isRequestParameter('module', 'event')->
+  isRequestParameter('action', 'list')
 ;
