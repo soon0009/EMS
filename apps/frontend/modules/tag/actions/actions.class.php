c4b74d4bdf5abbc749b26fafa7674b3425647218
Nigel Soon <ns@openradical.com>
2009-06-05 14:29:55 +0930

    Refactor EventPeer::getTaggedEvents($tag)

--- apps/frontend/modules/tag/actions/actions.class.php
+++ apps/frontend/modules/tag/actions/actions.class.php
@@ -20,28 +20,7 @@ class tagActions extends sfActions
     $c->add(TagPeer::NORMALIZED_TAG, $this->getRequestParameter('tag'));
     $this->tag = TagPeer::doSelectOne($c);
 
-    $conn = Propel::getConnection();
-    $query = '
-      SELECT DISTINCT(event.*)
-      FROM event
-      JOIN event_tag ON (event.id = event_tag.event_id)
-      JOIN tag ON (tag.id = event_tag.tag_id)
-      JOIN etime ON (event.id = etime.event_id)
-      JOIN etime_tag ON (etime.id = etime_tag.etime_id)
-      JOIN tag t ON (t.id = etime_tag.tag_id)
-      WHERE
-        tag.normalized_tag = ?
-        OR
-        t.normalized_tag = ?
-
-    ';
-
-    $stmt = $conn->prepareStatement($query);
-    $stmt-> setString(1, $this->getRequestParameter('tag'));
-    $stmt-> setString(2, $this->getRequestParameter('tag'));
-    $resultset = $stmt->executeQuery(ResultSet::FETCHMODE_NUM);
-		$this->events = EventPeer::populateObjects($resultset);
-
+		$this->events = EventPeer::getTaggedEvents($this->getRequestParameter('tag'));
     $this->forward404Unless($this->events);
   }
 }
--- lib/model/EventPeer.php
+++ lib/model/EventPeer.php
@@ -9,4 +9,46 @@
  */ 
 class EventPeer extends BaseEventPeer
 {
+  public static function getTaggedEvents($tag) {
+    $conn = Propel::getConnection();
+    $query = '
+      SELECT DISTINCT(%s.*)
+      FROM %s as event
+      JOIN %s event_tag ON (%s = %s)
+      JOIN %s tag ON (%s = %s)
+      JOIN %s etime ON (%s = %s)
+      JOIN %s etime_tag ON (%s = %s)
+      JOIN %s t ON (t.id = %s)
+      WHERE
+        tag.normalized_tag = ?
+        OR
+        t.normalized_tag = ?
+
+    ';
+
+    $query = sprintf($query,
+      EventPeer::TABLE_NAME,
+      EventPeer::TABLE_NAME,
+      EventTagPeer::TABLE_NAME,
+      EventPeer::ID,
+      EventTagPeer::EVENT_ID,
+      TagPeer::TABLE_NAME,
+      TagPeer::ID,
+      EventTagPeer::TAG_ID,
+      EtimePeer::TABLE_NAME,
+      EventPeer::ID,
+      EtimePeer::EVENT_ID,
+      EtimeTagPeer::TABLE_NAME,
+      EtimePeer::ID,
+      EtimeTagPeer::ETIME_ID,
+      TagPeer::TABLE_NAME,
+      EtimeTagPeer::TAG_ID
+    );
+
+    $stmt = $conn->prepareStatement($query);
+    $stmt-> setString(1, $tag);
+    $stmt-> setString(2, $tag);
+    $resultset = $stmt->executeQuery(ResultSet::FETCHMODE_NUM);
+		return EventPeer::populateObjects($resultset);
+  }
 }
