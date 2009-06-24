240bdc2beeae5d32b8ce23a3a76d06d167dfcfcd
Nigel Soon <ns@openradical.com>
2009-06-24 11:07:02 +0930

    Edit event - allow change of title without giving title exists error

--- lib/mySlugValidator.class.php
+++ lib/mySlugValidator.class.php
@@ -13,12 +13,15 @@ class mySlugValidator extends sfValidator
 
   public function execute(&$value, &$error) {
     $slug = myTools::stripText($value);
+    $event_id_param = $this->getParameter('event_id'); 
+
+    $event_id = $this->getContext()->getRequest()->getParameter($event_id_param); 
 
     $c = new Criteria();
     $c->add(EventPeer::SLUG, $slug);
     $event_slug = EventPeer::doSelectOne($c);
 
-    if ($event_slug) {
+    if ($event_slug && $event_slug->getId() != $event_id) {
       $error = $this->getParameter('slug_error');
       return false;
     }
