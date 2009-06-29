d9c2ccd3fa0d5a39ee64387f9963c0edc2cd8062
Nigel Soon <ns@openradical.com>
2009-06-29 12:11:36 +0930

    Start backend

---
+++ apps/backend/modules/etime/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * etime actions.
+ *
+ * @package    ems
+ * @subpackage etime
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class etimeActions extends autoetimeActions
+{
+}
