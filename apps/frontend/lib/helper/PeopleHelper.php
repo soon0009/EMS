e6851766e24a695069d1043848ae7a814a105c45
Nigel Soon <ns@openradical.com>
2009-07-28 09:13:15 +0930

    Javascript to add a organiser

--- apps/frontend/lib/helper/PeopleHelper.php
+++ apps/frontend/lib/helper/PeopleHelper.php
@@ -1,4 +1,38 @@
 <?php
+use_helper('Javascript');
+
+function person_item($type, $name="", $email="", $phone="") {
+  $tag = "<li>\n";
+  $tag .= "Name: <input type='text' name='event[$type][name][]' value='$name' />";
+  $tag .= "<br>Email: <input type='text' name='event[$type][email][]' value='$email' />";
+  $tag .= "<br>Phone: <input type='text' name='event[$type][phone][]' value='$phone' />";
+  $tag .= "</li>\n";
+  return $tag;
+}
+
+function add_person_item($id, $type) {
+  return update_element_function($id, array(
+    'position' => 'before',
+    'content' => person_item($type),
+  ));
+}
+
+function people_tag($object, $method, $type) {
+  $type = strtolower($type);
+  $tag = "";
+  $people = call_user_func(array($object, $method));
+  $tag .= "<ul>\n";
+  foreach ($people as $person) {
+    if (strtolower($person->getPersonType()) == $type) {
+      $tag .= person_item($type, $person->getName(), $person->getEmail(), $person->getPhone());
+    }
+  }
+  $tag .= person_item($type);
+  $tag .= "<li id='link_to_add_person'>".link_to_function("Add another $type", add_person_item('link_to_add_person', $type))."</li>";
+  $tag .= "</ul>\n";
+
+  return $tag;
+}
 
 function list_people($object, $method, $type) {
   $list = "";
--- apps/frontend/modules/event/templates/_edit.php
+++ apps/frontend/modules/event/templates/_edit.php
@@ -7,28 +7,30 @@
     <div class="value">
       <?php echo object_input_tag($event, 'getTitle', array ( 'class' => 'long',)) ?>
     </div>
+    <div class="clear_float"></div>
 
+    <label class="label">Organisers</label>
+    <div class="value">
+      <?php echo people_tag($event, 'getEventPeoples', 'Organiser') ?>
+    </div>
     <div class="clear_float"></div>
 
     <label class="label" for="description">Description</label>
     <div class="value">
       <?php echo object_textarea_tag($event, 'getDescription', array ( 'size' => '30x3',)) ?>
     </div>
-
     <div class="clear_float"></div>
 
     <label class="label" for="notes">Notes</label>
     <div class="value">
       <?php echo object_textarea_tag($event, 'getNotes', array ( 'size' => '30x3',)) ?>
     </div>
-
     <div class="clear_float"></div>
 
     <label class="label" for="notes">Tags</label>
     <div class="value">
       <?php echo object_input_tag($event, 'getTagString', array ( 'class' => 'long',)) ?>
     </div>
-
     <div class="clear_float"></div>
 
     <?php echo form_error('image_url'); ?>
@@ -36,28 +38,24 @@
     <div class="value">
       <?php echo object_input_tag($event, 'getImageUrl', array ( 'class' => 'long',)) ?>
     </div>
-
     <div class="clear_float"></div>
 
     <label class="label" for="status">Status</label>
     <div class="value">
       <?php echo object_select_tag($event, 'getStatusId', array ( 'related_class' => 'Status', 'include_blank' => true,)) ?>
     </div>
-
     <div class="clear_float"></div>
 
     <label class="label" for="status">Category</label>
     <div class="value">
       <?php echo object_select_tag($event, 'getCategoryId', array ( 'related_class' => 'Category', 'include_blank' => true,)) ?>
     </div>
-
     <div class="clear_float"></div>
 
     <label class="label" for="media_potential">Media potential</label>
     <div class="value">
       <?php echo object_checkbox_tag($event, 'getMediaPotential', array ()) ?>
     </div>
-
     <div class="clear_float"></div>
 
   </div>
